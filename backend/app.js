3de4f7c1c32a55c4f197ea740d7d8bca48f0a5ca
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-04-10 20:43:40 +0530

    adding .env in backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+require('dotenv').config();
+
 
 
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(cors());
 
-mongoose.connect("mongodb+srv://Raman:sani123@cluster0.o7dew.mongodb.net/employes",{
+mongoose.connect(process.env.CONNECTION_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
